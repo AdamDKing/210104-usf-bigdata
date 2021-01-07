1c87c9de6f159c9de22d1ea0702e28b5ec91996c
Adam King <adamdentonking@gmail.com>
2021-01-07 12:50:42 -0500

    wccli with basic regex for commands

--- week1/wordcountcli/src/main/scala/com/revature/wccli/Cli.scala
+++ week1/wordcountcli/src/main/scala/com/revature/wccli/Cli.scala
@@ -1,5 +1,8 @@
 package com.revature.wccli
 
+import scala.io.StdIn
+import scala.util.matching.Regex
+
 /**
   * A CLI that allows the user to interact with our application
   * 
@@ -12,6 +15,16 @@ package com.revature.wccli
   *
   */
 class Cli {
+
+  /**
+    * commandArgPattern is a regular expression (regex) that will help us
+    * extract the command and argument from user input on the command line
+    * 
+    * Regex is a tool used for pattern matching strings.  Lots of languages and other tools
+    * support regex.  It's good to learn at least the basic, but you can also just use this
+    * code for your project if you like.
+    */
+  val commandArgPattern : Regex = "(\\w+)\\s*(.*)".r
 
   /**
     * Prints a greeting to the user
@@ -37,6 +50,22 @@ class Cli {
   def menu() : Unit = {
     printWelcome()
     printOptions()
+    //this is the first version, not on a loop
+    // take user input using StdIn.readLine
+    // readLine is *blocking* which means that it pauses program execution while it waits for input
+    // this is fine for us, but we do want to take note.
+    val input = StdIn.readLine()
+    // Here's an example using our regex above, feel free to just follow along with similar commands and args
+    input match {
+      case commandArgPattern(cmd, arg) if cmd.equalsIgnoreCase("echo") => {
+        println(arg)
+      }
+      case commandArgPattern(cmd, arg) => {
+        println(s"Failed to parse command: $cmd with arguments: $arg")
+      } 
+
+    }
+
   }
 
 }
