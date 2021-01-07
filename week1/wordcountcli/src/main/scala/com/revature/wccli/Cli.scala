71c709f4440e4825b8f8f7a6dc0fdda745872b78
Adam King <adamdentonking@gmail.com>
2021-01-07 14:25:08 -0500

    continued CLI, looping menu

--- week1/wordcountcli/src/main/scala/com/revature/wccli/Cli.scala
+++ week1/wordcountcli/src/main/scala/com/revature/wccli/Cli.scala
@@ -3,69 +3,86 @@ package com.revature.wccli
 import scala.io.StdIn
 import scala.util.matching.Regex
 
-/**
-  * A CLI that allows the user to interact with our application
-  * 
+/** A CLI that allows the user to interact with our application
+  *
   * This Cli is a class because in the future we might provide customization options
   * that can be set when creating a new Cli instance.
-  * 
+  *
   * This Cli class will contain all our logic involving interacting with the user
   * we don't want all of our classes to be able to receive input from the command line
   * or write to the command line.  Instead, we'll have (almost) all that happen here.
-  *
   */
 class Cli {
 
-  /**
-    * commandArgPattern is a regular expression (regex) that will help us
+  /** commandArgPattern is a regular expression (regex) that will help us
     * extract the command and argument from user input on the command line
-    * 
+    *
     * Regex is a tool used for pattern matching strings.  Lots of languages and other tools
     * support regex.  It's good to learn at least the basic, but you can also just use this
     * code for your project if you like.
     */
-  val commandArgPattern : Regex = "(\\w+)\\s*(.*)".r
+  val commandArgPattern: Regex = "(\\w+)\\s*(.*)".r
 
-  /**
-    * Prints a greeting to the user
+  /** Prints a greeting to the user
     */
   def printWelcome(): Unit = {
     println("Welcome to Word Count CLI!")
   }
 
-  /**
-    * Prints the commands available to the user
+  /** Prints the commands available to the user
     */
   def printOptions(): Unit = {
     println("Commands available:")
+    println("exit : exits Word Count CLI")
+    println("Bonus Commands:")
     println("echo [string to repeat] : repeats a string back to the user")
+    println("addFive [number] : returns the given number, plus 5")
   }
 
-  /**
-    * This runs the menu, this is the entrypoint to the Cli class.
-    * 
+  /** This runs the menu, this is the entrypoint to the Cli class.
+    *
     * The menu will interact with the user on a loop and call other methods/classes
     * in order to achieve the result of the user's commands
     */
-  def menu() : Unit = {
+  def menu(): Unit = {
     printWelcome()
-    printOptions()
-    //this is the first version, not on a loop
-    // take user input using StdIn.readLine
-    // readLine is *blocking* which means that it pauses program execution while it waits for input
-    // this is fine for us, but we do want to take note.
-    val input = StdIn.readLine()
-    // Here's an example using our regex above, feel free to just follow along with similar commands and args
-    input match {
-      case commandArgPattern(cmd, arg) if cmd.equalsIgnoreCase("echo") => {
-        println(arg)
+    var continueMenuLoop = true
+    while (continueMenuLoop) {
+      printOptions() 
+      // take user input using StdIn.readLine
+      // readLine is *blocking* which means that it pauses program execution while it waits for input
+      // this is fine for us, but we do want to take note.
+      val input = StdIn.readLine()
+      // Here's an example using our regex above, feel free to just follow along with similar commands and args
+      input match {
+        case commandArgPattern(cmd, arg) if cmd.equalsIgnoreCase("echo") => {
+          println(arg)
+        }
+        case commandArgPattern(cmd, arg) if cmd.equalsIgnoreCase("exit") => {
+          continueMenuLoop = false
+        }
+        case commandArgPattern(cmd, arg) if cmd.equalsIgnoreCase("addfive") => {
+          addFive(arg)
+        }
+        case commandArgPattern(cmd, arg) => {
+          println(s"""Failed to parse command: "$cmd" with arguments: "$arg"""")
+        }
+        case _ => {
+          println("Failed to parse any input")
+        }
       }
-      case commandArgPattern(cmd, arg) => {
-        println(s"Failed to parse command: $cmd with arguments: $arg")
-      } 
-
     }
+    println("Thank you for using Word Count CLI, goodbye!")
+  }
 
+  def addFive(arg: String) = {
+    try {
+      println(s"result: ${arg.toDouble + 5.0}")
+    } catch {
+      case nfe: NumberFormatException => {
+        println(s"""Failed to parse "$arg" as a double""")
+      }
+    }
   }
 
 }
