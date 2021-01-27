3c4dc5aa6e9e23fa8773a6b7a059877cf155346c
Adam King <adamdentonking@gmail.com>
2021-01-27 11:03:39 -0500

    samplr start

---
+++ week4/samplr/build.sbt
@@ -0,0 +1,20 @@
+import Dependencies._
+
+ThisBuild / scalaVersion := "2.13.4"
+ThisBuild / version := "0.1.0-SNAPSHOT"
+ThisBuild / organization := "com.revature"
+ThisBuild / organizationName := "revature"
+
+lazy val root = (project in file("."))
+  .settings(
+    name := "samplr",
+    libraryDependencies += scalaTest % Test,
+    libraryDependencies += "org.apache.hadoop" % "hadoop-common" % "2.7.7",
+    libraryDependencies += "org.apache.hadoop" % "hadoop-mapreduce-client-core" % "2.7.7"
+  )
+
+  // a bit of magic, ty Tyler
+assemblyMergeStrategy in assembly := {
+  case PathList("META-INF", xs @ _*) => MergeStrategy.discard
+  case x                             => MergeStrategy.first
+}
---
+++ week4/samplr/src/main/scala/com/revature/samplr/SamplrDriver.scala
@@ -0,0 +1,34 @@
+package com.revature.samplr
+
+import org.apache.hadoop.mapreduce.Job
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat
+import org.apache.hadoop.fs.Path
+
+
+object SamplrDriver {
+  def main(args : Array[String]): Unit = {
+    
+    if (args.length != 3) {
+      println("Usage: samplrjar <fraction> <input> <output> ")
+      System.exit(-1)
+    }
+
+    val frac = args(0).toDouble
+
+    if (frac <= 0 || frac >= 1) {
+      println(s"Input fraction of ${frac} must be between 0 and 1")
+    }
+
+    val job = Job.getInstance()
+    job.setJarByClass(SamplrDriver.getClass())
+    job.setJobName("Samplr")
+    job.setInputFormatClass(classOf[TextInputFormat])
+
+    FileInputFormat.setInputPaths(job, new Path(args(1)))
+    FileOutputFormat.setOutputPath(job, new Path(args(2)))
+
+
+  }
+}
